b3d3662290310752f0df98c813b2d456bea11b80
Shehzad Akbar <akbar.shehzad@gmail.com>
2023-08-18 16:29:34 -0600

    added files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// page/_app.tsx
+import { ClerkProvider,RedirectToSignIn, SignedIn, SignedOut } from "@clerk/nextjs";
+import type { AppProps } from "next/app";
+ 
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ClerkProvider afterSignInUrl="/signedoutprovider">
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 }
+ 
+export default MyApp;
+
